49d76abf4525879c2685bb42d1a26a9abd2e5c4a
GDWR <gregory.dwr@gmail.com>
2024-01-21 19:24:01 +0000

    Remove SSE

---
+++ components/ShortensCounter.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="text-center py-4">
+    <span v-if="pending">Loading...</span>
+    <span v-else>Urls Shortened: {{ data?.shortens }}</span>
+    </div>
+</template>
+
+
+<script setup>
+const { pending, data } = useLazyFetch('/api/shortens');
+</script>
--- server/api/shortens.get.ts
+++ server/api/shortens.get.ts
@@ -2,28 +2,9 @@ import Redis from 'ioredis';
 
 const runtimeConfig = useRuntimeConfig();
 const redis = new Redis({ host: runtimeConfig.redisHost });
-const subRedis = new Redis({ host: runtimeConfig.redisHost });
 
 export default defineEventHandler(async (event) => {
-    setHeader(event, 'cache-control', 'no-cache');
-    setHeader(event, 'connection', 'keep-alive');
-    setHeader(event, 'content-type', 'text/event-stream');
-    setResponseStatus(event, 200);
-
-    let counter = 0
-
-    const sendEvent = (data: any) => {
-      event.node.res.write(`id: ${++counter}\n`);
-      event.node.res.write(`data: ${JSON.stringify(data)}\n\n`);
-    }
-    
-    await subRedis.subscribe('shorten');
-    sendEvent({ shortens: await redis.get(`shorten:count`) });
-
-    subRedis.on("message", (_, message) => {
-        var data = JSON.parse(message);
-        sendEvent({ shortens: data.shortens });
-      });
-
-    event._handled = true;
-})
+  return {
+    shortens: await redis.get(`shorten:count`),
+  };
+});
